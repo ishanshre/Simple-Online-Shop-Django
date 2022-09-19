55b7ba6b1b4562a43cd532bf4389113eeb1a8215
Ishan Shrestha <ishan.shrestha356@gmail.com>
2022-09-19 15:46:52 +0545

    Create a cart.py which contains cart class that manages the shopping cart with using session cookie method

--- cart/cart.py
+++ cart/cart.py
@@ -11,6 +11,27 @@ class Cart:
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
     
+
+    def __iter__(self):
+        '''iterate over the items in cart and get product from database'''
+        product_ids = self.cart.keys()
+        '''get the product object and add them to cart'''
+        products = Product.objects.filter(id__in=product_ids)
+        cart = self.cart.copy()
+        for product in products:
+            cart[str(product.id)]['product'] = product
+        for item in cart.values():
+            item['price'] = Decimal(item(['price']))
+            item['total_price'] = item['price']*item['quantity']
+            yield item
+    
+    def __len__(self):
+        #count all the items in the cart
+        return sum(item['quantity'] for item in self.cart.values())
+
+
+
+
     def add(self, product, quantity=1, override_quantity = False):
         product_id = str(product.id)
         if product_id not in self.cart:
@@ -18,4 +39,25 @@ class Cart:
                 'quantity': 0,
                 'price':str(product.price)
             }
-            
+        if override_quantity:
+            self.cart[product_id]['quantity']= quantity
+        else:
+            self.cart[product_id]['quantity'] += quantity
+            self.save()
+
+
+    def save(self):
+        self.session.modified = True
+    
+    def remove(self, product):
+        product_id = str(product.id)
+        if product_id in self.cart:
+            del self.cart[product_id]
+            self.save()
+
+    def clear(self):
+        del self.session[settings.CART_SESSION_ID]
+        self.save()
+
+    def get_total_price(self):
+        return sum(Decimal(item['price'])* item['quantity'] for item in self.cart.values())
